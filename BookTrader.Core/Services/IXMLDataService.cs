5307e2b20ed02d7cd2657349232cf97c455f4679
Andrey <andreytumaikin@yandex.ru>
2021-02-04 16:39:28 +0500

    Раразботка страниц справочников

---
+++ BookTrader.Core/Services/IXMLDataService.cs
@@ -0,0 +1,13 @@
+﻿using BookTrader.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookTrader.Core.Services
+{
+    public interface IXMLDataService
+    {
+        Task<IEnumerable<Account>> GetAccountsDataAsync();
+    }
+}
--- BookTrader.Core/Services/XMLDataService.cs
+++ BookTrader.Core/Services/XMLDataService.cs
@@ -7,39 +7,51 @@ using System.Threading.Tasks;
 
 namespace BookTrader.Core.Services
 {
-    public class XMLDataService : ISampleDataService
+    public class XMLDataService : IXMLDataService
     {
-        private IEnumerable<SampleOrder> AllOrders()
-        {
-            /*// The following is order summary data
-            var companies = AllCompanies();
-            return companies.SelectMany(c => c.Orders);*/
-            using (Context context = new Context())
-            {
-                var orders = context.SampleOrders.ToList();
-                return orders;
-            }
-        }
-
-        private IEnumerable<SampleCompany> AllCompanies()
-        {
-            using (Context context=new Context())
-            {
-                var companies = context.SampleCompanies.ToList(); ;
-                return companies;
-            }            
-        }
-
-        public async Task<IEnumerable<SampleOrder>> GetGridDataAsync()
+        public async Task<IEnumerable<Account>> GetAccountsDataAsync()
         {
             await Task.CompletedTask;
-            return AllOrders();
+            return AllAccounts();
         }
 
-        public async Task<IEnumerable<SampleOrder>> GetMasterDetailDataAsync()
+        private IEnumerable<Account> AllAccounts()
         {
-            await Task.CompletedTask;
-            return AllOrders();
+            throw new NotImplementedException();
         }
+
+
+        //private IEnumerable<SampleOrder> AllOrders()
+        //{
+        //    /*// The following is order summary data
+        //    var companies = AllCompanies();
+        //    return companies.SelectMany(c => c.Orders);*/
+        //    using (Context context = new Context())
+        //    {
+        //        var orders = context.SampleOrders.ToList();
+        //        return orders;
+        //    }
+        //}
+
+        //private IEnumerable<SampleCompany> AllCompanies()
+        //{
+        //    using (Context context=new Context())
+        //    {
+        //        var companies = context.SampleCompanies.ToList(); ;
+        //        return companies;
+        //    }            
+        //}
+
+        //public async Task<IEnumerable<SampleOrder>> GetGridDataAsync()
+        //{
+        //    await Task.CompletedTask;
+        //    return AllOrders();
+        //}
+
+        //public async Task<IEnumerable<SampleOrder>> GetMasterDetailDataAsync()
+        //{
+        //    await Task.CompletedTask;
+        //    return AllOrders();
+        //}
     }
 }
