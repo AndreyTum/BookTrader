9e557762cb9d233cec659b78ec280c247397318f
Andrey <andreytumaikin@yandex.ru>
2021-04-21 10:01:40 +0500

    Изменение справочника счетов

--- BookTrader/ViewModels/AccountsDataGridViewModel.cs
+++ BookTrader/ViewModels/AccountsDataGridViewModel.cs
@@ -1,9 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
-
+using System.Windows.Input;
 using BookTrader.Core.Models;
 using BookTrader.Core.Services;
-
+using Prism.Commands;
 using Prism.Windows.Mvvm;
 using Prism.Windows.Navigation;
 
@@ -13,7 +14,7 @@ namespace BookTrader.ViewModels
     {
         private readonly IXMLDataService xmlDataService;
 
-        public ObservableCollection<Account> Source { get; set; } = new ObservableCollection<Account>();        
+        public ObservableCollection<Account> Source { get; set; } = new ObservableCollection<Account>();
 
         private bool isVisibleCloseAccounts;
 
@@ -29,17 +30,49 @@ namespace BookTrader.ViewModels
             }
         }
 
+        //private ICommand saveCommand;
+
+        //public ICommand SaveCommand
+        //{
+        //    get
+        //    {
+        //        using (Context context = new Context())
+        //        {
+        //            context.SaveChanges();
+        //        }
+        //        return saveCommand;
+        //    }
+
+        //}
+
+        public DelegateCommand<object> SaveCommand { get; private set; }
+
+
 
         public AccountsDataGridViewModel(IXMLDataService xmlDataService)
         {
             this.xmlDataService = xmlDataService;
+            SaveCommand = new DelegateCommand<object>(Save, CanSave);
+        }
+
+        private bool CanSave(object parameter)
+        {
+            return true;
+        }
+
+        private void Save(object parameter)
+        {
+            using (Context context = new Context())
+            {
+                context.SaveChanges();
+            }
         }
 
         public override async void OnNavigatedTo(NavigatedToEventArgs e, Dictionary<string, object> viewModelState)
         {
             base.OnNavigatedTo(e, viewModelState);
             Source.Clear();
-                        
+
             var data = await xmlDataService.GetAccountsDataAsync();
 
 
